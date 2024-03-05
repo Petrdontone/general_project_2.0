d27498359345c534372fc22f40e5240ca667aec6
Peter Yasnetsov <13101013PR@gmail.com>
2024-03-05 21:56:10 +0300

    delete script

--- RAM_memory.sh
+++ RAM_memory.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
 
-# Получаем данные о памяти с помощью команды free
-memory_info=$(free -m)
-
-# Извлекаем строки, содержащие информацию об использовании памяти
-memory_usage=$(echo "$memory_info" | grep Mem)
-
-# Извлекаем значения из строки, используя awk
-total_memory=$(echo "$memory_usage" | awk '{print $2}')
-used_memory=$(echo "$memory_usage" | awk '{print $3}')
-free_memory=$(echo "$memory_usage" | awk '{print $4}')
-
-# Выводим результат
-echo "Общий объем памяти: $total_memory MB"
-echo "Использовано: $used_memory MB"
-echo "Свободно: $free_memory MB"
