964dc1fc7378387906a648cc15900e75a8e35d1d
Vladimir Mochalov <Raket5420@gmail.com>
2024-03-05 21:45:09 +0300

    Обновил скрипт

--- RAM_memory.sh
+++ RAM_memory.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-# Получаем данные о памяти с помощью команды free
-memory_info=$(free -m)
+# Читаем информацию о памяти из /proc/meminfo
+mem_info=$(</proc/meminfo)
 
-# Извлекаем строки, содержащие информацию об использовании памяти
-memory_usage=$(echo "$memory_info" | grep Mem)
+# Извлекаем нужные данные
+total_memory=$(echo "$mem_info" | grep -i "MemTotal" | awk '{print $2}')
+free_memory=$(echo "$mem_info" | grep -i "MemFree" | awk '{print $2}')
+buffers=$(echo "$mem_info" | grep -i "Buffers" | awk '{print $2}')
+cached=$(echo "$mem_info" | grep -i "^Cached" | awk '{print $2}')
+used_memory=$((total_memory - free_memory - buffers - cached))
 
-# Извлекаем значения из строки, используя awk
-total_memory=$(echo "$memory_usage" | awk '{print $2}')
-used_memory=$(echo "$memory_usage" | awk '{print $3}')
-free_memory=$(echo "$memory_usage" | awk '{print $4}')
+# Переводим байты в мегабайты для удобства чтения
+total_memory_mb=$((total_memory / 1024))
+used_memory_mb=$((used_memory / 1024))
+free_memory_mb=$((free_memory / 1024))
 
 # Выводим результат
-echo "Общий объем памяти: $total_memory MB"
-echo "Использовано: $used_memory MB"
-echo "Свободно: $free_memory MB"
+echo "Общий объем памяти: $total_memory_mb MB"
+echo "Использовано: $used_memory_mb MB"
+echo "Свободно: $free_memory_mb MB"
